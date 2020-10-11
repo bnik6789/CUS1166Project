65d0ccb4ce8d4c2a9527596ab45f047b33d5739a
Benjamin Nikolai <benjaminn.nikolai18@stjohns.edu>
2020-10-10 21:02:23 -0400

    Added classes and JFrame Object

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,5 +1,24 @@
 package GUI;
-//
-public class Main {
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/** 
+ * Class to display GUI of Cloud System
+ * @author 
+ * 
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("FrameDemo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Vehicular Cloud");
+		frame.setSize(300, 400);
+		frame.setVisible(true);
+				
+	}
+	
 }
